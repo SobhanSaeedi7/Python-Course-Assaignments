7ef731babb3f8ddf7a1656147f285d2f986ee20c
SobhanSaeedi7 <Sobi.S.K2007@gmail.com>
2023-02-26 22:24:23 +0330

    Part 1 (spl exercises)

---
+++ Homework-20/sql exercises/Insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO customers(id, name, city, country)
+VALUES(01,"SobhanSaeedi", "Golbahar", "Iran"),
+(02, "SadraBahari", "Mashhad", "Iran"),
+(03, "PeterParker", "Washengthons", "USA"),
+(04, "SaeedMaroof", "Fenerbaghche", "Turkey");
+
+
+INSERT INTO products(id, name, price, count)
+VALUES(01, "Barcelona outfit suit", 120000, 32),
+(02, "MoltenBalanceBall", 760000, 10),
+(03, "LegoSet76107", 450000, 5),
+(04, "FantesyUSB", 300000, 15);
